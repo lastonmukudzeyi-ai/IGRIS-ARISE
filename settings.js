b559aadb13eba8996c27e0610275260dfb73f4a8
lastonmukudzeyi-ai <lastonmukudzeyik@gmail.com>
2026-03-01 07:51:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'IGRIS',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "IGRIS THE BLOODRED",
+  botOwner: 'TMx93 起きる', // Your name
+  ownerNumber: '263777391977', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
